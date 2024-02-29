9397f897828fd5a3ef6e4f2a3556c86bca851d36
subhasishnath45 <subhasishnath45@gmail.com>
2024-02-29 23:40:33 +0530

    React Query: Just changed the server port.

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.delete('/api/tasks/:id', (req, res) => {
 
 app.use((req, res) => res.status(404).send('Route does not exist'));
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 const startApp = () => {
   try {
